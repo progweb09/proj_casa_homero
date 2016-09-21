a37a653a8b6b387fe240c91d21abff2874ec21e1
julindres <tengo3cmdepito@gmail.com>
2016-09-20 21:12:30 -0300

    dffgdhfghfhf

--- proj_casahomero/login.php
+++ proj_casahomero/login.php
@@ -9,7 +9,14 @@ $sql="call login_usuario(?,?,@valor_existe)";
 $stmt = $con->prepare($sql);
 $stmt->bind_param('ss', $usuario, $pass);
 $stmt->execute();
-
+$result2=$con->query("SELECT @valor_existe");
+$row=$result2->fetch_assoc();
+if ($row['@valor_existe']==0)
+	echo "<h1 style='text-align: center'>"."Ingreso invalido al sistema"."</h1>";
+			else 
+			{
+				
+			}
 
 
 
